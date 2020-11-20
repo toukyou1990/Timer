f36839e762b04009cad3e57a84816803db73e394
toukyou1990 <74651706+toukyou1990@users.noreply.github.com>
2020-11-20 21:38:03 +0900

    コミットテスト

--- Timer/ContentView.swift
+++ Timer/ContentView.swift
@@ -25,12 +25,10 @@ struct ContentView: View {
         
       
         
-        ZStack {
+    ZStack {
             Color.black
                 .opacity(0.02)
                 .edgesIgnoringSafeArea(.all)
-            
-        
        
     VStack(){
         
@@ -56,41 +54,43 @@ struct ContentView: View {
                 .padding(.leading, 24)
                 .frame(maxWidth: .infinity, alignment: .leading)
                 
-            TextField("Next Action", text: $textField )
+            TextField("NextActionaaa", text: $textField )
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .font(.system(size: 40, weight: .bold, design: .default ))
                 .padding(.horizontal, 24)
-        }
-
-      
-            HStack(spacing: 25) {
-                
-                Label("\(isActive ? "Stop" : "Play")", systemImage: "\(isActive ? "Pause.fill" : "play.fill")")
-                    .foregroundColor(isActive ? .red : .yellow).font(.title).onTapGesture(perform: {
-                        isActive.toggle()
+       
+            Label("\(isActive ? "Stop" : "Play")", systemImage: "\(isActive ? "Pause.fill" : "play.fill")")
+            .foregroundColor(isActive ? .red : .yellow).font(.title).onTapGesture(perform: {
+                isActive.toggle()
+            })
+            Label("Resume", systemImage: "stop.fill")
+                .foregroundColor(.red).font(.title).onTapGesture(perform: {
+                isActive = false
+                timeRemaining = defaultTimeRemaining
                 })
-                
-                                Label("Resume", systemImage: "stop.fill")
-                                    .foregroundColor(.red).font(.title).onTapGesture(perform: {
-                                    isActive = false
-                                    timeRemaining = defaultTimeRemaining
-                })
-                }
+            }
                 }.onReceive(timer, perform: { _ in guard isActive else { return }
                     if timeRemaining > 0 {
                         timeRemaining -= 1
                     } else {
                         isActive = false
                         timeRemaining = defaultTimeRemaining
-                
-                        
-                }
-            })
+                    }
+                })
+            }
         }
-    }
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        }
     }
 }
-}
+
+//▼道筋(仮)
+
+//- 60秒のカウントダウンを3時間に変更
+//- カウントダウンのカラーを30分以下で赤色
+//- テキスト入力後Startボタンをタップするとカウントダウン開始
+//- カウントダウン中のStartボタンをDeleateに変更
+//- ResumeをDeleatボタンに変更。タップするとデリートダイアログが表示され(テキスト削除とカウントリセット)
