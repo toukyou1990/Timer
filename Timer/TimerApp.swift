c6cb797fe46d73862a50ba30e7c3f9f3194250ad
toukyou1990 <74651706+toukyou1990@users.noreply.github.com>
2020-11-20 17:55:53 +0900

    Initial Commit

---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by Notty on 2020/11/18.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
